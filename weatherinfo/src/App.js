99829d4251188a95b3ea61b2133bf22633f5e741
Pooja patel <poojavpatel71@gmail.com>
2018-05-16 00:08:06 +0530

    no idea

--- weatherinfo/src/App.js
+++ weatherinfo/src/App.js
@@ -28,7 +28,7 @@ class App extends Component {
     e.preventDefault();
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${api_key}`);
+    const api_call = await fetch(`https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${api_key}`);
     const data = await api_call.json();
     if(city && country){
       console.log(data);  
