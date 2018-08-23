a99c07ca1a12b8f9cf3b47db2aba067238fa362f
Pooja patel <32796407+Poojavpatel@users.noreply.github.com>
2018-08-23 14:54:42 +0530

    removed api key

--- weatherinfo/src/App.js
+++ weatherinfo/src/App.js
@@ -4,11 +4,6 @@ import Titles from "./components/Titles"
 import Form from "./components/Form"
 import Weather from "./components/Weather"
 
- const api_key = "20622a2e98d5703ef596241796343d24";
-// var apicall = http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={APIKEY} 
-// var apicallciy = api.openweathermap.org/data/2.5/weather?q=London
-// var ApiCallCountry = http://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=20622a2e98d5703ef596241796343d24
-// var finalapicall = http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${api_key}
 
 class App extends Component {
 
